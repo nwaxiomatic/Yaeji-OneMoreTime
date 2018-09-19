f99e260dab9c0d34e899dac3237ddf45ace6e72a
mac <nicholasdwoodward@gmail.com>
2018-09-19 14:13:02 -0400

    mobile added v1

--- static/js/script-webgl.js
+++ static/js/script-webgl.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var isMobile = false; //initiate as false
+    // device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+console.log(isMobile);
+
 function resizeVideo(){
     var h = window.innerHeight;
     var w = window.innerWidth;
@@ -9,12 +15,65 @@ function resizeVideo(){
     var koreanOnemore = $('.korean-onemore');
     var body = $('body');
 
-    var centerSizeNum = 70;
+    var mobileText = $('.mobile-listen');
+
+    if(isMobile){
+      var centerSizeNum = 100;
     var centerSize = centerSizeNum.toString();
     var centerSizeHalf = (centerSizeNum/2).toString();
     var sizeNum = 45;
         var size = sizeNum.toString();
         var ratio = h/w;
+
+      if(h > w){
+        var amountT = (sizeNum*w*.4/100).toString();
+        var amountL = ((1 - sizeNum/100 + .1)*w).toString();
+        canvas.css({
+            'height' : size + 'vw',
+            'width' : (sizeNum * 9/5).toString() + 'vw',
+            'top' : amountT + 'px',
+            'left' : amountL + 'px',
+        })
+        centerImgB.css({
+            'width' : centerSize + 'vh',
+            'height' : centerSize + 'vh',
+        })
+
+        mobileText.css({
+            'width': '50vw',
+            'height': '25vw',
+            'bottom': '2em',
+            'left': '2em',
+        })
+      }
+        else {
+          var amountT = (sizeNum*h*.4/100).toString();
+        var amountL = (w - (sizeNum/100 - .1)*h).toString();
+            canvas.css({
+                'top' : amountT + 'px',
+            'left' : amountL + 'px',
+                'height' : size + 'vh',
+                'width' : (sizeNum * 9/5).toString() + 'vh',
+            })
+            centerImgB.css({
+                'width' : centerSize + 'vw',
+                'height' : centerSize + 'vw',
+            })
+            mobileText.css({
+            'width': '30vw',
+            'height': '15vw',
+            'bottom': '1em',
+            'left': '1em',
+        })
+        }
+      }
+    else{
+      var centerSizeNum = 70;
+          var centerSize = centerSizeNum.toString();
+          var centerSizeHalf = (centerSizeNum/2).toString();
+          var sizeNum = 45;
+            var size = sizeNum.toString();
+            var ratio = h/w;
 
     if(h > w){
       bezierBaseVals[4] = .95;
@@ -25,8 +84,6 @@ function resizeVideo(){
         canvas.css({
             'width' : size + 'vw',
             'height' : (sizeNum * 5/9).toString() + 'vw',
-        })
-        canvas.css({
             'top' : amountT + 'px',
             'left' : '78vw',
         })
@@ -73,6 +130,7 @@ function resizeVideo(){
             'height': '10vh',
         })
     }
+  }
 
     var canvas = $('#c');
   uniforms['u_resolution'] = [canvas.width(), canvas.height()];
@@ -84,7 +142,7 @@ var gl = twgl.getWebGLContext(document.getElementById("c"));
 var programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
 
 var imgCount = 0;
-var imgWait = 2;
+var imgWait = isMobile ? 1 : 2;
 
 function showPage(){
   if(imgCount==imgWait){
@@ -94,13 +152,9 @@ function showPage(){
   }
 }
 
-
-var textures = twgl.createTextures(gl, {
-  yellow: { src: "static/media/img/slimebg.jpg" },
-  water: { src: "static/media/img/slime2.png" },
-  clouds: { src: "static/media/img/noise.jpg" },
-}, function(){
-    var imgT = new Image();
+function setUpDesktop(){
+  $('.mobile').remove();
+  var imgT = new Image();
     var imgB = new Image();
     imgT.onload = function() { 
       imgCount ++;
@@ -114,7 +168,36 @@ var textures = twgl.createTextures(gl, {
     }
     imgT.src = "static/media/img/yaeji-cover-small-top.jpg"; 
     imgB.src = "static/media/img/yaeji-cover-small.jpg";      
-  });
+}
+
+function setUpMobile(){
+  $('.desktop').remove();
+  $('#c').css({'mix-blend-mode': 'difference'})
+    var imgB = new Image();
+    imgB.onload = function() { 
+      imgCount ++;
+      $('.center-image-bottom').attr('src', imgB.src);
+      showPage();
+    }
+    imgB.src = "static/media/img/yaeji-cover-small.jpg";    
+  showPage();
+}
+
+
+var textures = twgl.createTextures(gl, {
+  yellow: { src: "static/media/img/slimebg.jpg" },
+  water: { src: "static/media/img/slime.png" },
+  waterR: { src: "static/media/img/slime2.png" },
+  clouds: { src: "static/media/img/noise.jpg" },
+}, function(){
+  if(isMobile){
+    setUpMobile();
+  }
+  else {
+    setUpDesktop();
+  }
+}
+);
 
 var arrays = {
   position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
@@ -126,6 +209,7 @@ var uniforms = {
     resolution: [gl.canvas.width, gl.canvas.height],
 
     u_waterMap: textures.water,
+    u_waterRMap: textures.water,
     u_textureBg: textures.yellow,
     u_textureFg: textures.yellow,
 
@@ -140,12 +224,14 @@ var uniforms = {
 
     u_renderShine: true,
     u_renderShadow: false, 
+    u_isMobile: isMobile,
 
     u_resolution: [900, 500],
     u_textureRatio: .4,
 
-    u_mouse: [.5,.5],
+    u_mouse: [0,1],
     u_corner: [.2, .2],
+    u_pixelRatio:window.devicePixelRatio,
 
   };
 
@@ -187,7 +273,8 @@ function render(time) {
 
   requestAnimationFrame(render);
 
-
+  if(!isMobile){
   clearLine(lineCanvas);
   drawRandomBezier(uniforms['time']);
+}
 }
